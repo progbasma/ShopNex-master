dad312eb3764fcc5342675f8d4f98e9323832969
progbasma <prog.basma@gmail.com>
2025-10-01 22:58:39 +0300

    upload

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -11,8 +11,8 @@ const Popular = () => {
             <h1 className={`h1p_${theme}`}>POPULAR IN WOMEN</h1>
             <hr className={`hrp_${theme}`} />
             <div className="popular-item">
-                {data_product.map((item,i)=>{
-                    return<Item key={i}  id={item.id}   name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+                {data_product.map((item)=>{
+                    return <Item key={item.id}  id={item.id}   name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
                 })}
             </div>
         </div>
